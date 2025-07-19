0d415a3e0d7ac77abf0a1d2985225dbf92c0c5f3
mehrabhossaindev <mehrabs1933@gmail.com>
2025-07-19 22:20:24 +0600

    Delete mysql-test-data directory

--- mysql-test-data/01-schema.sql.sql
+++
@@ -1,8 +0,0 @@
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS `laravel_test`;
-
--- Create user and grant privileges
-CREATE USER IF NOT EXISTS 'laravel'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON `laravel_test`.* TO 'laravel'@'%';
-GRANT PROCESS ON *.* TO 'laravel'@'%';  -- Needed for Laravel migrations
-FLUSH PRIVILEGES;
