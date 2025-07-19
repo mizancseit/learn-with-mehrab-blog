6cb6f291d4b681cbfb6ee75c44042d01dce50217
mehrabhossaindev <mehrabs1933@gmail.com>
2025-07-19 21:23:06 +0600

    Update 01-schema.sql.sql

--- mysql-test-data/01-schema.sql.sql
+++ mysql-test-data/01-schema.sql.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS `laravel_test`;
 
 -- Create user and grant privileges
-CREATE USER IF NOT EXISTS 'laravel_user'@'%' IDENTIFIED BY 'secure_password';
-GRANT ALL PRIVILEGES ON `laravel_test`.* TO 'laravel_user'@'%';
-GRANT PROCESS ON *.* TO 'laravel_user'@'%';  -- Needed for Laravel migrations
+CREATE USER IF NOT EXISTS 'laravel'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON `laravel_test`.* TO 'laravel'@'%';
+GRANT PROCESS ON *.* TO 'laravel'@'%';  -- Needed for Laravel migrations
 FLUSH PRIVILEGES;
